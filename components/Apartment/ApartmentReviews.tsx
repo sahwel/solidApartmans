23bd82deb7f3aaca57819e7c69dfcb936d0f76e8
Mezősi József Krisztián <mezosijoci@gmail.com>
2022-03-16 19:47:30 +0100

    fix apartment in md size

--- components/Apartment/ApartmentReviews.tsx
+++ components/Apartment/ApartmentReviews.tsx
@@ -2,7 +2,7 @@ import ApartmentReview from './ApartmentReview'
 
 const ApartmentReviews = () => {
   return (
-    <div className="bg-white p-3 lg:col-start-3 lg:max-h-full lg:w-1/3 lg:pb-0">
+    <div className="bg-white p-3 md:rounded-bl-2xl md:rounded-br-2xl lg:col-start-3 lg:max-h-full lg:w-1/3 lg:rounded-none lg:pb-0">
       <h1 className="text-lg font-bold lg:hidden">Reviews</h1>
       <div className="mt-5 space-y-5 lg:mt-0 lg:h-full  lg:overflow-y-auto lg:pr-2">
         <ApartmentReview />
