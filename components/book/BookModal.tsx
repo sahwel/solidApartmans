d5c4a84977fb37319a9751594928b329132bbcb4
Mezősi József Krisztián <mezosijoci@gmail.com>
2022-03-20 16:26:54 +0100

    added tooltip to 'want to play total?', added funcionalty to book modal

--- components/book/BookModal.tsx
+++ components/book/BookModal.tsx
@@ -1,5 +1,12 @@
-import { FunctionComponent, memo } from 'react'
+import {
+  Dispatch,
+  FunctionComponent,
+  memo,
+  SetStateAction,
+  useState,
+} from 'react'
 import { Button } from '../Button'
+import Tooltip from '@mui/material/Tooltip'
 
 interface BookModalProps {
   numberOfAdults: number
@@ -7,9 +14,16 @@ interface BookModalProps {
   arrive: string
   left: string
 }
+const handleChange =
+  (setter: Dispatch<SetStateAction<boolean>>, newState: boolean) => () =>
+    setter(newState)
 
 const BookModal: FunctionComponent<BookModalProps> = memo(
   function BookModal({ arrive, left, numberOfAdults, numberOfKids }) {
+    const [isTotal, setIsTotal] = useState(false)
+    const [accpect, setAccpect] = useState(false)
+    const [isCreditCard, setIsCreditCard] = useState(true)
+
     return (
       <div className="mt-3 space-y-5">
         <div className="grid grid-cols-2">
@@ -21,6 +35,8 @@ const BookModal: FunctionComponent<BookModalProps> = memo(
         <div>
           <div className="flex items-center justify-start">
             <input
+              onChange={handleChange(setIsTotal, !isTotal)}
+              checked={isTotal}
               type="checkbox"
               id="paymentTotal"
               className="mr-2 h-4 w-4 border-main-blue"
@@ -29,15 +45,22 @@ const BookModal: FunctionComponent<BookModalProps> = memo(
               <label htmlFor="paymentTotal">
                 Do you want to pay the total?
               </label>
-              <span className="ml-2 flex h-[1.2rem] w-[1.2rem] cursor-pointer items-center justify-center rounded-full bg-main-blue text-white">
-                ?
-              </span>
+              <Tooltip
+                title=" You can pay the 10% or the 100% of the total, however you you
+                are only able these options"
+              >
+                <span className="ml-2 flex h-[1.2rem] w-[1.2rem] cursor-pointer items-center justify-center rounded-full bg-main-blue text-white ">
+                  ?
+                </span>
+              </Tooltip>
             </span>
           </div>
           <div className="flex space-x-3">
             <div className="flex items-center justify-start">
               <input
+                checked={isCreditCard}
                 type="checkbox"
+                onChange={handleChange(setIsCreditCard, true)}
                 id="creditCard"
                 className="mr-2 h-4 w-4 border-main-blue"
               />
@@ -47,6 +70,8 @@ const BookModal: FunctionComponent<BookModalProps> = memo(
               <input
                 type="checkbox"
                 id="bankTransfer"
+                checked={!isCreditCard}
+                onChange={handleChange(setIsCreditCard, false)}
                 className="mr-2 h-4 w-4 border-main-blue"
               />
               <label htmlFor="bankTransfer">Bank transfer</label>
@@ -56,11 +81,13 @@ const BookModal: FunctionComponent<BookModalProps> = memo(
             <input
               type="checkbox"
               id="houseRules"
+              checked={accpect}
+              onChange={handleChange(setAccpect, !accpect)}
               className="mr-2 h-4 w-4 border-main-blue"
             />
             <label htmlFor="houseRules">
               Do you accept the<span className="font-bold"> house roules</span>
-              and the <span className="font-bold"> privacy policy</span>?
+              &nbsp; and the <span className="font-bold"> privacy policy</span>?
             </label>
           </div>
         </div>
